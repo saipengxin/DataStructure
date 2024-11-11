a7653b9854630017ef609bab96cee97e8f041cd2
saipx <saipengxin@163.com>
2024-11-11 12:15:09 +0800

    静态链表

--- 线性表/List_3/List_3.go
+++ 线性表/List_3/List_3.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
+
 // 静态链表 (用数组实现的链表)
 
 const (
-	MaxSize = 1000
+	MaxSize = 10
 	ERROR   = -1
 	OK      = 1
 )
@@ -13,49 +15,98 @@ type ListNode struct {
 	cur  int // 后继元素下标存储
 }
 
-var List [MaxSize]ListNode
+type List struct {
+	maxSize int
+	length  int
+	list    []ListNode
+}
 
-func InitList() int {
-	for i := 0; i < MaxSize-1; i++ {
-		List[i].cur = i + 1
+func InitList(size int) (list List) {
+	list = List{
+		maxSize: size,
+		length:  0,
+		list:    make([]ListNode, size),
 	}
-	List[MaxSize-1].cur = 0 // 初始化为空链表，最后一个元素的cur为0
-	return OK
+
+	for i := 0; i < size-1; i++ {
+		list.list[i].cur = i + 1
+	}
+	list.list[size-1].cur = 0 // 初始化为空链表，最后一个元素的cur为0
+	return list
 }
 
 // Malloc_SSL 返回插入元素的放置位置
-func Malloc_SSL() int {
-	i := List[0].cur // 获取插入元素的放置位置
+func (list *List) Malloc_SSL() int {
+	i := list.list[0].cur // 获取插入元素的放置位置
 	if i != 0 {
-		List[0].cur = List[i].cur // 新元素插入，备用链表的第一个元素要移动到下一个元素
+		list.list[0].cur = list.list[i].cur // 新元素插入，备用链表的第一个元素要移动到下一个元素
 	}
 	return i
 }
 
-func ListLength() int {
-	j := 0
-	i := List[MaxSize-1].cur
-	for i != 0 {
-		i = List[i].cur
-		j++
-	}
-	return j
+func (list *List) ListLength() int {
+	return list.length
 }
 
-func ListInsert(i int, e int) int {
-	if i < 1 || i > ListLength()+1 {
+func (list *List) ListInsert(i int, e int) int {
+	if i < 1 || i > list.ListLength()+1 {
 		return ERROR
 	}
-	j := Malloc_SSL()
+	k := list.maxSize - 1
+	j := list.Malloc_SSL()
 	if j != 0 {
-		List[j].data = e
-		k := 0
-		for i := 1; i <= i-1; i++ {
-			k = List[k].cur
+		list.list[j].data = e
+		// 找到要操作位置的前一个元素
+		for l := 1; l <= i-1; l++ {
+			k = list.list[k].cur
 		}
-		List[j].cur = List[k].cur
-		List[k].cur = j
+		list.list[j].cur = list.list[k].cur
+		list.list[k].cur = j
+
+		list.length = list.length + 1
 		return OK
 	}
 	return ERROR
+}
+
+func (list *List) ListDelete(i int) int {
+	if i < 1 || i > list.ListLength()+1 {
+		return ERROR
+	}
+	k := list.maxSize - 1
+	// 找到要操作位置的前一个元素
+	for j := 1; j <= i-1; j++ {
+		k = list.list[k].cur
+	}
+	// 跳过要删除元素的下标
+	list.list[k].cur = list.list[list.list[k].cur].cur
+	list.length = list.length - 1
+	return OK
+}
+
+func main() {
+	list := InitList(MaxSize)
+	fmt.Println("==============================")
+	fmt.Printf("%#v\n", list)
+	fmt.Println("==============================")
+
+	list.ListInsert(1, 10)
+	fmt.Println("==============================")
+	fmt.Printf("%#v\n", list)
+	fmt.Println("==============================")
+
+	list.ListInsert(1, 20)
+	fmt.Println("==============================")
+	fmt.Printf("%#v\n", list)
+	fmt.Println("==============================")
+
+	list.ListInsert(2, 55)
+	fmt.Println("==============================")
+	fmt.Printf("%#v\n", list)
+	fmt.Println("==============================")
+
+	list.ListDelete(1)
+	fmt.Println("==============================")
+	fmt.Printf("%#v\n", list)
+	fmt.Println("==============================")
 }
