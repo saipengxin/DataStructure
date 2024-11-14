bc7599c71732b6dca916a37fc7382d5679472ba7
saipx <saipengxin@163.com>
2024-11-14 20:03:18 +0800

    栈

--- 栈/链栈/List.go
+++ 栈/链栈/List.go
@@ -10,9 +10,24 @@ type List struct {
 	top   *ListNode
 }
 
+const (
+	ERROR = -1
+	OK    = 1
+)
+
 func NewList() *List {
 	return &List{
 		count: 0,
-		top:   nil,
+		top:   nil, // 栈顶，null的时候为空栈
 	}
+}
+
+func (list *List) Push(e int) int {
+	node := &ListNode{
+		data: e,
+		next: list.top,
+	}
+	list.top = node
+	list.count++
+	return OK
 }
